4e680bcb046d06602da9873c7436f046d022169c
sindagalai <galai.senda@gmail.com>
2026-04-06 22:13:48 +0100

    gitleaks config file in secret sca

--- vars/gitleaksScan.groovy
+++ vars/gitleaksScan.groovy
@@ -4,10 +4,17 @@ def call() {
 
         echo "Running Gitleaks scan..."
 
+        pwd
+        ls -la
+        ls -la .gitleaks.toml
+
         gitleaks dir . \
           --config=.gitleaks.toml \
           --report-format=json \
           --report-path=reports/sast/gitleaks-report.json || true
+
+        echo "Gitleaks report content:"
+        cat reports/sast/gitleaks-report.json
     '''
 }
 
